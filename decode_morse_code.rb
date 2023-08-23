401a73e69d42dd2a6a739ac6cff1e7f707071e3c
PabloBona <pablobonasera@gmail.com>
2023-08-23 18:01:12 -0300

    add constant & decode_char method

---
+++ decode_morse_code.rb
@@ -0,0 +1,16 @@
+class MorseDecoder
+  MORSE_CODE = { # Capital letters beacuse it's a constant
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z', ' ' => ' '
+  }.freeze
+
+  # Create a method to decode a Morse code character, takes a string, and return
+  # the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+  def decode_char(char)
+    MORSE_CODE[char]
+  end
+end
